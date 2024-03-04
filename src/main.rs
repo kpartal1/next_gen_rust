8c3817d41eb45d965b9815e550ce4b5b8b896fe5
Konstantine Partalas <kpartal1@trinity.edu>
2024-03-03 22:19:23 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+mod buffer;
+mod color;
+
+use buffer::Buffer;
+use color::Color;
+use minifb::{Key, Window, WindowOptions};
+use rand::Rng;
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 360;
+
+// FIXME TODO remove rand crate before finishing using it for testing
+
+fn main() {
+    let mut buffer = Buffer::new(WIDTH, HEIGHT);
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions {
+            resize: true,
+            ..Default::default()
+        },
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    // window.limit_update_rate(None);
+
+    let mut rng = rand::thread_rng();
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        buffer.clear();
+        // for i in buffer.iter_mut() {
+        //     *i = Color::RED;
+        // }
+        buffer.set_color(Color::random());
+        let x1 = rng.gen_range(0..WIDTH);
+        let y1 = rng.gen_range(0..HEIGHT);
+        let x2 = rng.gen_range(0..WIDTH);
+        let y2 = rng.gen_range(0..HEIGHT);
+        let x3 = rng.gen_range(0..WIDTH);
+        let y3 = rng.gen_range(0..HEIGHT);
+        buffer.tri((x1, y1), (x2, y2), (x3, y3));
+
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
+}
