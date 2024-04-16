27e3637b799b420fb1b3961e456529a7d0677742
trevortrues <123995584+trevortrues@users.noreply.github.com>
2024-04-16 14:24:04 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,62 +3,91 @@ mod canvas;
 mod color;
 mod linalg;
 
+use std::{thread, time};
 use canvas::Canvas;
 use color::Color;
-use linalg::vec2::Vec2;
-use linalg::{matrix::Mat4x4, vec4::Vec4};
-use minifb::{Key, Window, WindowOptions};
-use rand::Rng;
+use linalg::matrix::Mat4x4;
+use linalg::vec3::Vec3;
+use std::f32::consts::PI;
 
-const WIDTH: usize = 640;
-const HEIGHT: usize = 360;
+struct Cube {
+    vertices: [Vec3<f32>; 8],
+}
+
+impl Cube {
+    fn new() -> Self {
+        Cube {
+            vertices: [
+                Vec3::new(-1.0, -1.0, -1.0),
+                Vec3::new(1.0, -1.0, -1.0),
+                Vec3::new(1.0, 1.0, -1.0),
+                Vec3::new(-1.0, 1.0, -1.0),
+                Vec3::new(-1.0, -1.0, 1.0),
+                Vec3::new(1.0, -1.0, 1.0),
+                Vec3::new(1.0, 1.0, 1.0),
+                Vec3::new(-1.0, 1.0, 1.0),
+            ],
+        }
+    }
+
+    fn rotate(&mut self, angle: f32, _axis: Vec3<f32>) {  
+        let rotation_matrix = Mat4x4::rotation_y(angle);
+        for vertex in &mut self.vertices {
+            *vertex = rotation_matrix.transform_vector(*vertex);
+        }
+    }
+    
+}
+
+fn draw_cube(canvas: &mut Canvas, cube: &Cube, color: Color) {
+    for i in 0..cube.vertices.len() {
+        let start = cube.vertices[i];
+        let end = cube.vertices[(i + 1) % cube.vertices.len()];
+        let (sx, sy) = project_to_screen(start);
+        let (ex, ey) = project_to_screen(end);
+        canvas.draw_line(sx, sy, ex, ey, color); 
+    }
+}
+
+
+fn project_to_screen(vertex: Vec3<f32>) -> (usize, usize) {
+    let scale = 100.0; 
+    let screen_width = 800.0;  
+    let screen_height = 600.0;
+    let x = ((vertex.x * scale) + (screen_width / 2.0)) as usize;
+    let y = ((-vertex.z * scale) + (screen_height / 2.0)) as usize;  
+    (x, y)
+}
+
 
 fn main() {
-    let mut canvas = Canvas::new(WIDTH, HEIGHT);
-
-    let mut window = Window::new(
+    let mut window = minifb::Window::new(
         "Test - ESC to exit",
-        WIDTH,
-        HEIGHT,
-        WindowOptions {
-            resize: true,
-            ..Default::default()
-        },
-    )
-    .unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
+        800,
+        600,
+        minifb::WindowOptions::default(),
+    ).unwrap_or_else(|e| panic!("{}", e));
 
-    // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    let mut canvas = Canvas::new(800, 600);
+    let mut cube = Cube::new(); 
 
-    // All of this stuff is slightly wrong :)
-    // let model = Mat4x4::identity();
-    // let eye = Vec4::from(0., 0., 1., 0.);
-    // let center = Vec4::new();
-    // let up = Vec4::from(0., 1., 0., 0.);
-    // let view = Mat4x4::look_at(eye, center, up);
-    // let projection = Mat4x4::perspective(45., WIDTH as f32 / HEIGHT as f32, 0.1, 100.);
-    // let projection = Mat4x4::frustum(-1., 1., -1., 1., 1., 100.);
-    // let mvp = projection * view * model;
+    let mut angle = 0.0;
+    while window.is_open() && !window.is_key_down(minifb::Key::Escape) {
+        let axis = Vec3::new(0.0, 1.0, 0.0);  
+        angle += PI / 180.0;  
+        cube.rotate(angle, axis);
 
-    let mut rng = rand::thread_rng();
-    while window.is_open() && !window.is_key_down(Key::Escape) {
         canvas.clear();
-        canvas.set_color(Color::random());
-        let x1 = rng.gen_range(-1f32..1. + f32::EPSILON);
-        let y1 = rng.gen_range(-1f32..1. + f32::EPSILON);
-        let x2 = rng.gen_range(-1f32..1. + f32::EPSILON);
-        let y2 = rng.gen_range(-1f32..1. + f32::EPSILON);
-        let x3 = rng.gen_range(-1f32..1. + f32::EPSILON);
-        let y3 = rng.gen_range(-1f32..1. + f32::EPSILON);
-        // let (x1, y1) = (&mvp * Vec4::from(0., 0.5, 0., 0.)).xy();
-        // let (x2, y2) = (&mvp * Vec4::from(-0.5, -0.5, 0., 0.)).xy();
-        // let (x3, y3) = (&mvp * Vec4::from(0.5, -0.5, 0., 0.)).xy();
-        canvas.tri((x1, y1), (x2, y2), (x3, y3));
+        draw_cube(&mut canvas, &cube, Color::BLUE);
+        thread::sleep(time::Duration::from_millis(1));
+        draw_cube(&mut canvas, &cube, Color::GREEN);
+        thread::sleep(time::Duration::from_millis(3));
+        draw_cube(&mut canvas, &cube, Color::BLACK);
+        thread::sleep(time::Duration::from_millis(4));
+        draw_cube(&mut canvas, &cube, Color::RED);
+  
 
-        window
-            .update_with_buffer(canvas.buffer(), WIDTH, HEIGHT)
-            .unwrap();
+
+        window.update_with_buffer(canvas.buffer(), canvas.width(), canvas.height()).unwrap();
     }
 }
