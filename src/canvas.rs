c43c1761ca47ea72f85e4defc38b2935f7a9c558
trevortrues <123995584+trevortrues@users.noreply.github.com>
2024-04-16 14:22:37 -0500

    Update canvas.rs

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,60 +1,43 @@
 #![allow(unused)]
 
-use std::ops::{Deref, DerefMut};
-
 use crate::{buffer::Buffer, color::Color};
 
 pub struct Canvas {
     buf: Buffer,
+    width: usize,
+    height: usize,
 }
 
 impl Canvas {
     pub fn new(width: usize, height: usize) -> Self {
-        Canvas {
+        Self {
             buf: Buffer::new(width, height),
+            width,
+            height,
         }
     }
 
-    pub fn buffer(&self) -> &[u32] {
-        &self.buf
-    }
-
     pub fn clear(&mut self) {
-        self.buf.clear();
+        self.buf.clear(Color::WHITE);  
     }
 
-    pub fn fill(&mut self) {
-        self.buf.fill();
+    pub fn draw_pixel(&mut self, x: usize, y: usize, color: Color) {
+        self.buf.draw_pixel(x, y, color);
     }
 
-    pub fn set_color(&mut self, color: u32) {
-        self.buf.set_color(color);
+    pub fn draw_line(&mut self, x0: usize, y0: usize, x1: usize, y1: usize, color: Color) {
+        self.buf.draw_line(x0, y0, x1, y1, color);
     }
 
-    fn point_to_pixel(&self, (x, y): (f32, f32)) -> (usize, usize) {
-        let (width, height) = self.buf.size();
-        let x = (x + 1.) / 2.;
-        let y = (y + 1.) / 2.;
-        (
-            (x * (width - 1) as f32) as usize,
-            (height - 1) - (y * (height - 1) as f32) as usize,
-        )
+    pub fn buffer(&self) -> &[u32] {
+        self.buf.data()
     }
 
-    pub fn pixel(&mut self, index: (f32, f32)) {
-        self.buf.pixel(self.point_to_pixel(index));
+    pub fn width(&self) -> usize {
+        self.width
     }
 
-    pub fn line(&mut self, p1: (f32, f32), p2: (f32, f32)) {
-        self.buf
-            .line(self.point_to_pixel(p1), self.point_to_pixel(p2))
-    }
-
-    pub fn tri(&mut self, p1: (f32, f32), p2: (f32, f32), p3: (f32, f32)) {
-        self.buf.tri(
-            self.point_to_pixel(p1),
-            self.point_to_pixel(p2),
-            self.point_to_pixel(p3),
-        )
+    pub fn height(&self) -> usize {
+        self.height
     }
 }
